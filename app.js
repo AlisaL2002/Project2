632b774d326242fee007a57a28f2cb7580c03aa1
Alisa Logacheva <logacheva.alisa@gmail.com>
2024-03-11 23:59:14 -0400

    creared 8 Routes

--- app.js
+++ app.js
@@ -48,39 +48,37 @@ app.get('/post/postId/:postId', (req, res) => {
     res.json(post || {});
   });
 
-
+// Adding a New Post
   app.post('/post', (req, res) => {
     const newPost = req.body;
     const posts = readPostsSync();
-    posts.push({ ...newPost, comments: [] }); // Assume the client sends all required fields
+    posts.push({ ...newPost, comments: [] });
     savePostsSync(posts);
     res.status(201).send('Post added');
   });  
 
-// POST /post -  Adding new post
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Dleting a post
+// DELETE /post/:postId - Removes the post from the array.
+app.delete('/post/:postId', (req, res) => {
+  let posts = readPostsSync();
+  const { postId } = req.params;
+  posts = posts.filter(post => post.postId !== postId);
+  savePostsSync(posts);
+  res.send('Post deleted');
 });
 
-// DELETE /post/:postId - Removes the post from the array
-app.delete('/post/:postId', (req, res) => {
-    let posts = readPostsSync();
-    const { postId } = req.params;
-    posts = posts.filter(post => post.postId !== postId);
-    savePostsSync(posts);
-    res.send('Post deleted');
-  });
+// Getting comment for a post
 
-// GET /post/:postId/comments - Returns all comments associated with a post
-  app.get('/post/:postId/comments', (req, res) => {
+app.get('/post/:postId/comments', (req, res) => {
     const { postId } = req.params;
     const posts = readPostsSync();
     const post = posts.find(post => post.postId === postId);
     res.json(post ? post.comments : []);
   });
 
-// 
-app.post('/post/:postId/comment', (req, res) => {
+  // POST /post/:postId/comment - Adds a comment to the end of the post and returns an ID
+  // Adding Comment to a Post
+  app.post('/post/:postId/comment', (req, res) => {
     const { postId } = req.params;
     const newComment = req.body;
     const posts = readPostsSync();
@@ -93,6 +91,19 @@ app.post('/post/:postId/comment', (req, res) => {
       res.status(404).send('Post not found');
     }
   });
-  
-  
+
+// DELETE /post/:postId/comment/:commentId - Deletes the comment with the provided id
+// Deleting a comment
+  app.delete('/post/:postId/comment/:commentId', (req, res) => {
+    const { postId, commentId } = req.params;
+    const posts = readPostsSync();
+    const post = posts.find(post => post.postId === postId);
+    if (post) {
+      post.comments = post.comments.filter(comment => comment.commentId !== commentId);
+      savePostsSync(posts);
+      res.send('Comment deleted');
+    } else {
+      res.status(404).send('Post not found');
+    }
+  });
   
